6fc6e86453eebc4bd9296fb7282084ed0acb35fc
Harrison Hassig <harrisonhassig@gmail.com>
2022-12-17 23:02:06 +0100

    Ready for survey form submission

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -6,10 +6,29 @@ class SurveysController < ApplicationController
 
   def show
     @survey = Survey.find(params[:id])
-    # @venues = Venue.all
   end
 
   def index
     @surveys = Survey.all
+  end
+
+  def new
+    @survey = Survey.new
+  end
+
+  def create
+    @survey = Survey.new(survey_params)
+    @survey.user = current_user
+    if @survey.save
+      redirect_to survey_path(@survey), notice: 'Saved.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:user_id, :death, :life_threatening, :sig_consequences, :incorrect_with_consent, :intraoperative_course_change, :unanticipated_conversion, :aborted_incomplete, :unplanned_stoma, :unplanned_removal, :intervention, :post_op_care_change, :intensive_care, :re_operation, :blood_loss_high, :more_blood_units, :eauiaic, :iaeseverity, :suffixt, :modifiedsatava, :classintra, :eaes)
   end
 end
