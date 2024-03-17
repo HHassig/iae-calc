33233fbc81ecbcaa6c4e24f69e3749075ba9c86c
Harrison Hassig <harrisonhassig@gmail.com>
2024-03-17 22:19:12 +0100

    Refactoring

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,58 +1,69 @@
-require "csv"
+class SurveysController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :home ]
 
-class Survey < ApplicationRecord
-  belongs_to :user
-  def yes_or_no(answer)
-    answer ? "Yes" : "No"
+  def index
+    @surveys = Survey.where(user: current_user)
+    respond_to do |format|
+      format.html
+      format.csv { send_data as_csv(@surveys) }
+    end
   end
 
-  def get_eauiaic(survey)
-    return "0" if !survey.death && !survey.intervention && !survey.life_threatening && !survey.sig_consequences && !survey.incorrect_with_consent && !survey.unanticipated_conversion && !survey.aborted_incomplete && !survey.unplanned_stoma && !survey.unplanned_removal && !survey.re_operation
-    return "1" if (survey.intervention || survey.more_blood_units) && !survey.death && !survey.life_threatening && !survey.sig_consequences && !survey.incorrect_with_consent && !survey.unanticipated_conversion && !survey.aborted_incomplete && !survey.unplanned_stoma && !survey.unplanned_removal && !survey.re_operation
-    return "2" if (survey.unanticipated_conversion || survey.sig_consequences || survey.re_operation) && !survey.death && !survey.life_threatening && !survey.incorrect_with_consent && !survey.aborted_incomplete && !survey.unplanned_stoma && !survey.unplanned_removal
-    return "3" if !survey.death && survey.life_threatening && !survey.incorrect_with_consent && !survey.aborted_incomplete && !survey.unplanned_stoma && !survey.unplanned_removal
-    return "4A" if !survey.death && !survey.incorrect_with_consent && !survey.aborted_incomplete && !survey.unplanned_stoma && survey.unplanned_removal
-    return "4B" if (survey.aborted_incomplete || survey.unplanned_stoma) && !survey.death && !survey.incorrect_with_consent
-    return "5A" if !survey.death && survey.incorrect_with_consent
-    return "5B" if survey.death
-    return "No record for EAUIAIC."
+  def home
+    @survey = Survey.new
   end
 
-  def get_iae_severity(survey)
-    return "I" if !survey.death && !survey.intervention && !survey.incorrect_with_consent && !survey.unanticipated_conversion && !survey.aborted_incomplete && !survey.unplanned_stoma && !survey.unplanned_removal && !survey.re_operation
-    return "II" if !survey.death && survey.intervention && !survey.unanticipated_conversion && !survey.aborted_incomplete && !survey.unplanned_stoma && !survey.unplanned_removal && !survey.re_operation
-    return "III" if !survey.death && !survey.unanticipated_conversion && !survey.aborted_incomplete && survey.unplanned_removal && !survey.re_operation
-    return "IV" if !survey.death && (survey.unanticipated_conversion || survey.aborted_incomplete || survey.unplanned_stoma || survey.incorrect_with_consent) && !survey.re_operation
-    return "V" if !survey.death && survey.re_operation
-    return "VI" if survey.death
-    return "No record for iAE Severity."
+  def show
+    @survey = validate_survey(params[:id])
   end
 
-  def get_modified_satava(survey)
-    return "I" if !survey.death && !survey.life_threatening && !survey.sig_consequences && !survey.unanticipated_conversion && !survey.aborted_incomplete && !survey.unplanned_stoma && !survey.unplanned_removal && !survey.re_operation && !survey.blood_loss_high
-    return "II" if (survey.unanticipated_conversion || survey.unplanned_stoma || survey.unplanned_removal || survey.blood_loss_high || survey.aborted_incomplete) && !survey.death && !survey.life_threatening && !survey.sig_consequences
-    return "III" if survey.death || survey.life_threatening || survey.sig_consequences || survey.re_operation
-    return "No record for Modified Satava."
+  def new
+    @survey = Survey.new
   end
 
-  def get_class_intra(survey)
-    return "0" if !survey.death && !survey.life_threatening && !survey.sig_consequences && !survey.incorrect_with_consent && !survey.unanticipated_conversion && !survey.aborted_incomplete && !survey.unplanned_stoma && !survey.unplanned_removal && !survey.intervention && !survey.re_operation && !survey.blood_loss_high
-    return "I" if (survey.intraoperative_course_change || survey.blood_loss_high) && !survey.death && !survey.life_threatening && !survey.sig_consequences && !survey.unanticipated_conversion && !survey.aborted_incomplete && !survey.unplanned_stoma && !survey.unplanned_removal && !survey.intervention && !survey.re_operation
-    return "II" if (survey.intervention || survey.aborted_incomplete || survey.blood_loss_high || survey.re_operation || survey.incorrect_with_consent) && !survey.death && !survey.life_threatening && !survey.sig_consequences && !survey.unanticipated_conversion && !survey.unplanned_stoma && !survey.unplanned_removal
-    return "III" if (survey.life_threatening || survey.sig_consequences || survey.unplanned_stoma || survey.unanticipated_conversion || survey.unplanned_removal) && !survey.death
-    return "IV" if survey.death
-    return "No record for Class Intra."
+  def create
+    @survey = Survey.new(survey_params)
+    @survey.user = current_user
+    @survey.patient_id = "N/A" if @survey.patient_id == ""
+    @survey.iae_description = "Not provided." if @survey.iae_description == ""
+    @survey.iae_management = "Not provided." if @survey.iae_management == ""
+    if @survey.save
+      update_survey(@survey)
+      redirect_to survey_path(@survey), notice: "Saved."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def get_eaes(survey)
-    return "5" if survey.death
-    return "4" if survey.life_threatening || survey.sig_consequences || survey.re_operation || survey.intensive_care
-    return "3" if survey.post_op_care_change || survey.intraoperative_course_change
-    return "2" if survey.intervention && !survey.post_op_care_change
-    return "1"
+  private
+
+  def update_survey(survey)
+    survey.update!(
+      eauiaic: survey.get_eauiaic(survey),
+      iae_severity: survey.get_iae_severity(survey),
+      modified_satava: survey.get_modified_satava(survey),
+      class_intra: survey.get_class_intra(survey),
+      suffix_t: survey.get_suffix_t(survey),
+      eaes: survey.get_eaes(survey)
+    )
   end
 
-  def get_suffix_t(survey)
-    survey.more_blood_units ? " T" : ""
+  def survey_params
+    params.require(:survey).permit(:user, :death, :life_threatening, :sig_consequences, :incorrect_with_consent, :intraoperative_course_change, :unanticipated_conversion, :aborted_incomplete, :unplanned_stoma, :unplanned_removal, :intervention, :post_op_care_change, :intensive_care, :re_operation, :blood_loss_high, :more_blood_units, :eauiaic, :iae_severity, :suffix_t, :modified_satava, :class_intra, :eaes, :patient_id, :iae_description, :iae_management)
+  end
+
+  def as_csv(surveys)
+    CSV.generate do |csv|
+      columns = %w(id patient_id iae_description iae_management eauiaic iae_severity suffix_t modified_satava class_intra eaes
+        death life_threatening sig_consequences incorrect_with_consent intraoperative_course_change unanticipated_conversion aborted_incomplete
+        unplanned_stoma unplanned_removal intervention post_op_care_change intensive_care re_operation blood_loss_high more_blood_units)
+      csv << columns.map(&:humanize)
+      surveys.each { |survey| csv << survey.attributes.values_at(*columns) }
+    end
+  end
+
+  def validate_survey(id)
+    survey = Survey.find(id)
+    survey.user == current_user ? survey : nil
   end
 end
